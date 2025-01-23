6f5c4e7b9d6c8ea1b440fe42c79f1ff0cc3e3306
KyleMit <kmitofsky@stackoverflow.com>
2025-01-23 08:53:35 -0500

    scaffold app

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,58 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+  let types = [
+    { name: "Bug", symbol: "ഋ", alternates: "⁂ප" },
+    { name: "Dark", symbol: "☪", alternates: "☽" },
+    { name: "Dragon", symbol: "☬", alternates: "中" },
+    { name: "Electric", symbol: "ϟ", alternates: "☇☈⚡" },
+    { name: "Fairy", symbol: "✦", alternates: "❤" },
+    { name: "Fighting", symbol: "夫", alternates: "⛩☯" },
+    { name: "Fire", symbol: "♨", alternates: "㊋" },
+    { name: "Flying", symbol: "༄", alternates: "࿓✈⊱⊰" },
+    { name: "Ghost", symbol: "⚉", alternates: "ᗝ" },
+    { name: "Grass", symbol: "⸙", alternates: "㊍☘⚘" },
+    { name: "Ground", symbol: "ᨏ", alternates: "㊏⏚" },
+    { name: "Ice", symbol: "❅", alternates: "☃❄" },
+    { name: "Normal", symbol: "⦿", alternates: "✴" },
+    { name: "Poison", symbol: "☠", alternates: "☣" },
+    { name: "Psychic", symbol: "꩜", alternates: "ത⛧⛤❂ᯣ۞⚙᳀" },
+    { name: "Rock", symbol: "☗", alternates: "ᨒᨓᨏ" },
+    { name: "Steel", symbol: "✧", alternates: "㊎✦⚓" },
+    { name: "Water", symbol: "☂", alternates: "㊌≋⛈" },
+  ];
+  
+
+  async function copyToClipboard(event) {
+    try {
+      const value = event.target.value; // Access the input value
+      await navigator.clipboard.writeText(value);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Pogo Paste</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h2>Types</h2>
+  <table>
+    <tbody>
+      {#each types as { name, symbol }}
+      <tr>
+        <td>{name}</td>
+        <td>
+          <input type="button" value={symbol} on:click={copyToClipboard} />
+        </td>
+      </tr>
+    {/each}
+    </tbody>
+  </table>
+  
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
