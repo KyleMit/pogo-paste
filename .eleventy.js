a130d3775cbbf93ae703bc84419aae6c9ecd91de
KyleMit <kmitofsky@stackoverflow.com>
2026-02-25 09:09:03 -0500

    move symbols to data folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,6 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
 
-  eleventyConfig.addGlobalData("symbols", () => import("./src/symbols.json", { with: { type: "json" } }).then(m => m.default));
-
-
   return {
     dir: {
       input: "src",
